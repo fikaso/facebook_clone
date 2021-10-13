634bc4332c92a8802ed53f1fa24ed55d695a4812
fikaso <vranjes.filip@gmail.com>
2021-10-13 21:22:28 +0200

    minor

--- pages/index.js
+++ pages/index.js
@@ -36,7 +36,7 @@ export default function Home({ session }) {
   };
 
   return (
-    <div className="bg-gray-100 max-w-full overflow-y-auto">
+    <div className="bg-gray-100 max-w-full">
       <Head>
         <title>Facebook</title>
       </Head>
