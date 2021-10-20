72b84fe6f84721c8650380510a3f1be2440729d9
fikaso <vranjes.filip@gmail.com>
2021-10-20 17:49:04 +0200

    fixed error on story upload

--- components/Story.js
+++ components/Story.js
@@ -4,16 +4,20 @@ import { db } from "../firebase";
 import { useEffect, useState } from "react";
 
 function Story({ id, storyImage, elapsedTime, userImage }) {
-  const [storyElapsedTime, setStoryElapsedTime] = useState(false);
+  const [storyElapsedTime, setStoryElapsedTime] = useState(null);
 
   useEffect(() => {
-    const { res, isDate } = elapsedTime;
-    if (isDate) {
-      db.collection("stories").doc(id).delete();
-    } else {
-      setStoryElapsedTime(res);
+    try {
+      const { res, isDate } = elapsedTime;
+      if (isDate) {
+        db.collection("stories").doc(id).delete();
+      } else {
+        setStoryElapsedTime(res);
+      }
+    } catch (error) {
+      console.log(error);
     }
-  }, []);
+  }, [elapsedTime]);
 
   return (
     <Link
@@ -24,30 +28,32 @@ function Story({ id, storyImage, elapsedTime, userImage }) {
         },
       }}
     >
-      {storyImage && (
-        <div className="relative h-56 w-32 cursor-pointer overflow-x-auto p-3 transition-duration-200 transform ease-in hover:scale-105 hover:animate-pulse">
-          <Image
-            unoptimized={true}
-            className="absolute rounded-full z-50 top-10"
-            src={userImage}
-            width={40}
-            height={40}
-            objectFit="cover"
-          />
-          <Image
-            unoptimized={true}
-            className="object-cover brightness-75 rounded-xl"
-            src={storyImage}
-            layout="fill"
-          />
+      <div>
+        {storyImage && (
+          <div className="relative h-56 w-32 cursor-pointer overflow-x-auto p-3 transition-duration-200 transform ease-in hover:scale-105 hover:animate-pulse">
+            <Image
+              unoptimized={true}
+              className="absolute rounded-full z-50 top-10"
+              src={userImage}
+              width={40}
+              height={40}
+              objectFit="cover"
+            />
+            <Image
+              unoptimized={true}
+              className="object-cover brightness-75 rounded-xl"
+              src={storyImage}
+              layout="fill"
+            />
 
-          {storyElapsedTime && (
-            <p className="absolute top-2 right-2 text-white text-xl">
-              {storyElapsedTime}h
-            </p>
-          )}
-        </div>
-      )}
+            {storyElapsedTime !== null && (
+              <p className="absolute top-2 right-2 text-white text-xl">
+                {storyElapsedTime}h
+              </p>
+            )}
+          </div>
+        )}
+      </div>
     </Link>
   );
 }
